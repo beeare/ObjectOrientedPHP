14d4a72560f97fb7cb9e5b42e94ffcd726537168
Benjamin Rack <beeare@users.noreply.github.com>
2016-01-17 12:52:46 +0100

    Example 2 added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ObjectOrientedPHP
 This project is just to teach Object Oriented Principles in PHP.
+## Example1
+This example shows the creation of as well as the destruction of objects from a given class.
---
+++ tests/Example2/SimpleClassTest.php
@@ -0,0 +1,103 @@
+<?php
+namespace beeare\OOP\Example2;
+
+class SimpleClassTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNewInstance()
+    {
+        $obj = new SimpleClass();
+        $this->assertInstanceOf(SimpleClass::class, $obj);
+    }
+
+    public function testPublicPropertyAccess()
+    {
+        $obj = new SimpleClass();
+
+        $obj->publicProperty = 'test';
+        $this->assertEquals('test', $obj->publicProperty);
+    }
+
+    public function testProtectedPropertyAccess()
+    {
+        $this->expectOutputRegex('/.+Cannot access protected property.+/');
+
+        $obj = new SimpleClass();
+
+        try {
+            $obj->protectedProperty = 'test';
+        } catch (\Error $e) {
+            $this->assertRegExp('/.+Cannot access protected property.+/', $this->getActualOutput());
+        }
+    }
+
+    public function testPrivatePropertyAccess()
+    {
+        $this->expectOutputRegex('/.+Cannot access private property.+/');
+
+        $obj = new SimpleClass();
+
+        try {
+            $obj->privateProperty = 'test';
+        } catch (\Error $e) {
+            $this->assertRegExp('/.+Cannot access private property.+/', $this->getActualOutput());
+        }
+    }
+
+    public function testNonExistingPropertyAccess()
+    {
+        $obj = new SimpleClass();
+
+        $obj->nonExistingProperty = 'test';
+        $this->assertEquals('test', $obj->nonExistingProperty);
+    }
+
+    public function testPublicMethodCall()
+    {
+        $this->expectOutputString('Method can be called from everywhere.');
+
+        $obj = new SimpleClass();
+        $obj->publicMethod();
+
+        $this->assertEquals('Method can be called from everywhere.', $this->getActualOutput());
+    }
+
+    public function testProtectedMethodCall()
+    {
+        $this->expectOutputRegex('/.+Call to protected method.+from context.+/');
+
+        $obj = new SimpleClass();
+
+        try {
+            $obj->protectedMethod();
+        } catch (\Error $e) {
+            $this->assertRegExp('/.+Call to protected method.+from context.+/', $this->getActualOutput());
+        }
+    }
+
+    public function testPrivateMethodCall()
+    {
+        $this->expectOutputRegex('/.+Call to private method.+from context.+/');
+
+        $obj = new SimpleClass();
+
+        try {
+            $obj->privateMethod();
+        } catch (\Error $e) {
+            $this->assertRegExp('/.+Call to private method.+from context.+/', $this->getActualOutput());
+        }
+    }
+
+
+    public function testNonExistingMethodCall()
+    {
+        $this->expectOutputRegex('/.+Call to undefined method.+/');
+
+        $obj = new SimpleClass();
+
+        try {
+            $obj->nonExistingMethod();
+        } catch (\Error $e) {
+            $this->assertRegExp('/.+Call to undefined method.+/', $this->getActualOutput());
+        }
+    }
+}
